49ce6c743e9a3f205cd03cdc00a2205616b9e08f
Martin Schiaffino <martin.schiaffino@ubiome.com>
2019-10-15 10:53:47 -0300

    Add post listing item component draft

--- src/components/PostListing.vue
+++ src/components/PostListing.vue
@@ -4,17 +4,22 @@
       v-for="post in posts"
       :key="post.id"
     >
-      <span v-text="post.title"></span>
+      <post-listing-item :post-id="post.id"></post-listing-item>
     </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
+import PostListingItem from "./PostListingItem.vue";
+
 export default {
   name: "post-listing",
   computed: {
     ...mapGetters("postsStore", ["posts"])
+  },
+  components: {
+    PostListingItem
   }
 };
 </script>
---
+++ src/components/PostListingItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <el-card>
+    <span v-text="post.title"></span>
+  </el-card>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  props: {
+    postId: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    ...mapState("postsStore", ["posts"]),
+    post() {
+      return this.posts[this.postId];
+    }
+  }
+};
+</script>
+
+<style>
+</style>
