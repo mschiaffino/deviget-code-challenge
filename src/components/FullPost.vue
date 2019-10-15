81487a268ea0563324424105b593b529f2996f14
Martin Schiaffino <martin.schiaffino@ubiome.com>
2019-10-15 10:39:59 -0300

    Create draft of post view and full post component

---
+++ src/components/FullPost.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-card>
+    <span v-text="post.title"></span>
+  </el-card>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  name: "full-post",
+  props: {
+    postId: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    ...mapState("postsStore", ["posts"]),
+    post() {
+      return this.posts[this.postId];
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/Posts.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <full-post post-id="2hozly"></full-post>
+  </div>
+</template>
+
+<script>
+import FullPost from "../components/FullPost.vue";
+
+export default {
+  name: "posts",
+  components: {
+    FullPost
+  },
+  created() {
+    this.$store.dispatch("postsStore/fetchPosts");
+  }
+};
+</script>
+
+<style>
+</style>
